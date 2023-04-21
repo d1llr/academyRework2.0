db90bc755b7cc195b7e70d48cb2a62e0d4adfe25
Ярослав Лошкарев <dfdfdf.dfdf.1985@mail.ru>
2023-04-21 15:13:03 +0300

    testing

--- backend/frantsuz_api/urls.py
+++ backend/frantsuz_api/urls.py
@@ -1,11 +1,9 @@
-from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/', include('users.urls')),
@@ -24,16 +22,10 @@ schema_view = get_schema_view(
     permission_classes=[permissions.AllowAny],
 )
 urlpatterns += [
-        url(r'^swagger(?P<format>\.json|\.yaml)$',
-            schema_view.without_ui(cache_timeout=0), name='schema-json'),
-        url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-            name='schema-swagger-ui'),
-        url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-            name='schema-redoc'),
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'),
     ]
-# if settings.DEBUG:
-#     urlpatterns = (
-#         urlpatterns
-#         + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#         + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#     )
