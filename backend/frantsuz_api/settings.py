6702c5b194b91c7c062d16afca11244d2b02e51b
Ярослав Лошкарев <dfdfdf.dfdf.1985@mail.ru>
2023-04-24 11:34:14 +0300

    debug ON

--- backend/frantsuz_api/settings.py
+++ backend/frantsuz_api/settings.py
@@ -130,16 +130,15 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
-CORS_URLS_REGEX = r'^/api/.*$'
-# if DEBUG:
-#     CORS_ORIGIN_ALLOW_ALL = True
-# else:
 
-CORS_URLS_ALLOW_ALL = False
-CORS_ALLOWED_ORIGINS = [
-    'http://37.140.195.3:3000',
-    'http://localhost:3000',
-]
+CORS_URLS_REGEX = r'^/api/.*$'
+if DEBUG:
+    CORS_ORIGIN_ALLOW_ALL = True
+else:
+    CORS_ALLOWED_ORIGINS = [
+        'http://37.140.195.3:3000',
+        'http://localhost:3000',
+    ]
 
 DJOSER = {
     'LOGIN_FIELD': 'email',
